8f112d8ee2cf7b3e4309abb1d4e616e90f801119
Leonardo Salvador <leonardosalvador81@gmail.com>
2026-06-07 19:01:56 -0300

    Add Microsoft Clarity

--- assets/js/site.js
+++ assets/js/site.js
@@ -122,4 +122,14 @@
       window.scrollTo({ top: 0, behavior: 'smooth' });
     });
   }
+
+  /* Microsoft Clarity */
+(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);
+    t.async=1;
+    t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];
+    y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "x3ifr281wq");
 })();
